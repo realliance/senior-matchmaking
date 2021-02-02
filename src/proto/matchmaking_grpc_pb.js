3728e811f51f154ec17dac536e937e3502b90dbc
Meachamp <34523297+Meachamp@users.noreply.github.com>
2021-02-02 02:05:12 -0500

    Update protobuf generation module

--- src/proto/matchmaking_grpc_pb.js
+++ src/proto/matchmaking_grpc_pb.js
@@ -1,7 +1,7 @@
 // GENERATED CODE -- DO NOT EDIT!
 
 'use strict';
-var grpc = require('grpc');
+var grpc = require('@grpc/grpc-js');
 var proto_matchmaking_pb = require('../proto/matchmaking_pb.js');
 
 function serialize_AuthenticateRequest(arg) {

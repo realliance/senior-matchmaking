685fbbc5d24d9eede99165b89b0ed3d96f8ddd07
Meachamp <34523297+Meachamp@users.noreply.github.com>
2021-02-03 19:36:06 -0500

    Finish queue linkages

--- src/mmqueue.ts
+++ src/mmqueue.ts
@@ -1,13 +1,27 @@
-import {MMQClientUpdate, MMQServerUpdate} from './proto/matchmaking_pb'
+import {ConfirmResponse, MatchParameters, MMQClientUpdate, MMQServerUpdate, Status} from './proto/matchmaking_pb'
 import {Player} from './mmplayer'
-import {ServerDuplexStream} from '@grpc/grpc-js';
 
 export class MatchMakingQueue {
-    onPlayerUpdate(channel: ServerDuplexStream<MMQClientUpdate, MMQServerUpdate>, ply: Player) : void {
+    onPlayerConnected(ply: Player) : void {
+
+    }
+
+    onPlayerUpdate(req: MMQClientUpdate, ply: Player, write: (res: MMQServerUpdate) => boolean) : void {
 
     }
 
     onPlayerDisconnect(ply: Player) : void {
 
+    }
+
+    onPlayerConfirm(ply: Player) : ConfirmResponse {
+        let res: ConfirmResponse = new ConfirmResponse()
+        res.setStatus(Status.STATUS_OK)
+        return res
+    }
+
+    onPlayerRequestMatchParams(ply: Player) : MatchParameters {
+        let res: MatchParameters = new MatchParameters()
+        return res
     }
 }
--- src/mmserver.ts
+++ src/mmserver.ts
@@ -1,6 +1,7 @@
 import * as grpc from '@grpc/grpc-js';
 import {IMatchMakingServer} from './proto/matchmaking_grpc_pb';
 import {MatchMakingQueue} from './mmqueue'
+import {Player, getPlayerInfo} from './mmplayer'
 import * as mm from './proto/matchmaking_pb';
 
 let playerQueue: MatchMakingQueue = new MatchMakingQueue();
@@ -8,20 +9,25 @@ export class MatchMakingServer implements IMatchMakingServer {
     [method: string]: grpc.UntypedHandleCall
 
     async queue(call: grpc.ServerDuplexStream<mm.MMQClientUpdate, mm.MMQServerUpdate>) : Promise<void> {
-        call.on('data', (req: mm.MMQClientUpdate) => {
+        let ply: Player = await getPlayerInfo(call.metadata.getMap()['token'] as String)
+        playerQueue.onPlayerConnected(ply)
 
+        call.on('data', (req: mm.MMQClientUpdate) => {
+            playerQueue.onPlayerUpdate(req, ply, call.write)
         })
 
         call.on('end', () => {
-
+            playerQueue.onPlayerDisconnect(ply)
         })
     }
 
-    confirmMatch(call: grpc.ServerUnaryCall<mm.ConfirmRequest, mm.ConfirmResponse>, callback: grpc.sendUnaryData<mm.ConfirmResponse>) : void {
-
+    async confirmMatch(call: grpc.ServerUnaryCall<mm.ConfirmRequest, mm.ConfirmResponse>, callback: grpc.sendUnaryData<mm.ConfirmResponse>) : Promise<void> {
+        let ply: Player = await getPlayerInfo(call.metadata.getMap()['token'] as String)
+        callback(null, playerQueue.onPlayerConfirm(ply))
     }
 
-    getMatchParameters(call: grpc.ServerUnaryCall<mm.MatchParametersRequest, mm.MatchParameters>, callback: grpc.sendUnaryData<mm.MatchParameters>) : void {
-
+    async getMatchParameters(call: grpc.ServerUnaryCall<mm.MatchParametersRequest, mm.MatchParameters>, callback: grpc.sendUnaryData<mm.MatchParameters>) : Promise<void> {
+        let ply: Player = await getPlayerInfo(call.metadata.getMap()['token'] as String)
+        callback(null, playerQueue.onPlayerRequestMatchParams(ply))
     }
 }

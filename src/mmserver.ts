1c109a3e2e851b2c3b7a06809e299e0f9bee8791
Meachamp <34523297+Meachamp@users.noreply.github.com>
2021-02-07 03:02:21 -0500

    Add mmchannel and update match making server

--- src/mmserver.ts
+++ src/mmserver.ts
@@ -3,17 +3,23 @@ import {IMatchMakingServer} from './proto/matchmaking_grpc_pb';
 import {MatchMakingQueue} from './mmqueue'
 import {Player, getPlayerInfo} from './mmplayer'
 import * as mm from './proto/matchmaking_pb';
+import {MatchMakingSessions} from './mmsession'
 
 let playerQueue: MatchMakingQueue = new MatchMakingQueue();
+let sessions: MatchMakingSessions = new MatchMakingSessions();
 export class MatchMakingServer implements IMatchMakingServer {
     [method: string]: grpc.UntypedHandleCall
 
     async queue(call: grpc.ServerDuplexStream<mm.MMQClientUpdate, mm.MMQServerUpdate>) : Promise<void> {
-        let ply: Player = await getPlayerInfo(call.metadata.getMap()['token'] as String)
-        playerQueue.onPlayerConnected(ply)
+        let token: String = call.metadata.getMap()['token'] as String
+        let ply: Player = await getPlayerInfo(token)
+        playerQueue.onPlayerConnected(ply, {
+            write: call.write,
+            end: call.end
+        })
 
         call.on('data', (req: mm.MMQClientUpdate) => {
-            playerQueue.onPlayerUpdate(req, ply, call.write)
+            playerQueue.onPlayerUpdate(req, ply)
         })
 
         call.on('end', () => {
@@ -27,7 +33,8 @@ export class MatchMakingServer implements IMatchMakingServer {
     }
 
     async getMatchParameters(call: grpc.ServerUnaryCall<mm.MatchParametersRequest, mm.MatchParameters>, callback: grpc.sendUnaryData<mm.MatchParameters>) : Promise<void> {
-        let ply: Player = await getPlayerInfo(call.metadata.getMap()['token'] as String)
+        let token: String = call.metadata.getMap()['token'] as String
+        let ply: Player = await getPlayerInfo(token)
         callback(null, playerQueue.onPlayerRequestMatchParams(ply))
     }
 }

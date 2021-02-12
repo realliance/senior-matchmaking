34e1e91cdacdd6fd573cbb6770fcd01fabe824f7
Meachamp <34523297+Meachamp@users.noreply.github.com>
2021-02-12 01:51:52 -0500

    Styling changes

--- src/mmapi.ts
+++ src/mmapi.ts
@@ -4,8 +4,8 @@ import { Player } from './mmplayer';
 
 export const getPlayerInfo = async (token: string) : Promise<Player|null> => {
     const res = await superagent.get(
-        `${process.env.accountApi}/internal/users/${encodeURIComponent(token)}`,
-    ).accept('Application/json');
+        `${process.env.accountApi || ''}/internal/users/${encodeURIComponent(token)}`,
+    ).accept('application/json');
 
     if (res.status === 200 && res?.body?.user) {
         return { uid: res.body.user.id };
@@ -16,8 +16,8 @@ export const getPlayerInfo = async (token: string) : Promise<Player|null> => {
 
 export const notifyMatchInit = async (match: Match) : Promise<string|null> => {
     const res = await superagent.post(
-        `${process.env.accountApi}/internal/match`,
-    ).accept('Application/json')
+        `${process.env.accountApi || ''}/internal/match`,
+    ).accept('application/json')
         .send({
             match: {
                 users: match.players.map((ply: Player) => ply.uid),
--- src/tests/sessions.test.ts
+++ src/tests/sessions.test.ts
@@ -2,7 +2,7 @@ import { MatchMakingSessions } from '../mmsession';
 
 test('Can insert a session', async () => {
     const sessions: MatchMakingSessions = new MatchMakingSessions();
-    sessions.createSession('test_token', { uid: 0 });
+    sessions.createSession('test_token', { uid: '0' });
 
     expect(sessions.hasSession('test_token')).toBe(true);
 });

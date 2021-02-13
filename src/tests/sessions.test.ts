2a50d2de4e29b877ae248aad781a104b94c50a49
Meachamp <34523297+Meachamp@users.noreply.github.com>
2021-02-13 11:33:29 -0500

    Update session tests

--- src/tests/sessions.test.ts
+++ src/tests/sessions.test.ts
@@ -3,12 +3,12 @@ import * as mmapi from '../mmapi';
 
 jest.mock('../mmapi');
 const mockedmmapi = mmapi as jest.Mocked<typeof mmapi>;
-mockedmmapi.getPlayerInfo.mockResolvedValue({ uid: '0' });
 let sessions: MatchMakingSessions;
 const token = 'test_token';
 
 beforeEach(() => {
     sessions = new MatchMakingSessions();
+    mockedmmapi.getPlayerInfo.mockResolvedValue({ uid: '0' });
 });
 
 describe('Session storage functionality', () => {
@@ -21,6 +21,16 @@ describe('Session storage functionality', () => {
     test('initConnection creates a session', async () => {
         await sessions.initConnection(token, {} as PlayerConnection);
         expect(sessions.hasSession(token)).toBe(true);
+    });
+
+    test('initConnection denies session if player does not exist', async () => {
+        mockedmmapi.getPlayerInfo.mockResolvedValue(null);
+        const endMock = jest.fn();
+
+        const res = await sessions.initConnection(token, <unknown>{ end: endMock } as PlayerConnection);
+        expect(sessions.hasSession(token)).toBe(false);
+        expect(res).toBe(false);
+        expect(endMock).toHaveBeenCalled();
     });
 
     test('validateSession can retreive the player', async () => {
@@ -39,7 +49,7 @@ describe('Session storage functionality', () => {
         expect(Object.keys(sessions.sessions).length).toBe(0);
     });
 
-    test('validateSession can retreive the player', async () => {
+    test('validateSession returns null if session does not exist', async () => {
         expect(sessions.validateSession(token)).toBe(null);
     });
 });
@@ -47,20 +57,25 @@ describe('Session storage functionality', () => {
 describe('Session Management', () => {
     test('Duplicate session kicks earlier session', async () => {
         const endMock = jest.fn();
+
         const res = await sessions.initConnection(token, <unknown>{ end: endMock } as PlayerConnection);
         expect(res).toBe(true);
         expect(endMock).toBeCalledTimes(0);
+
         const res2 = await sessions.initConnection(token, <unknown>{ end: endMock } as PlayerConnection);
         expect(res2).toBe(true);
         expect(Object.keys(sessions.playerToToken).length).toBe(1);
         expect(Object.keys(sessions.sessions).length).toBe(1);
+
         expect(endMock).toBeCalledTimes(1);
     });
 
     test('Player can be kicked by token', async () => {
         const endMock = jest.fn();
+
         await sessions.initConnection(token, <unknown>{ end: endMock } as PlayerConnection);
         sessions.kickSessionByToken(token);
+
         expect(Object.keys(sessions.playerToToken).length).toBe(0);
         expect(Object.keys(sessions.sessions).length).toBe(0);
         expect(endMock).toBeCalled();
@@ -68,8 +83,10 @@ describe('Session Management', () => {
 
     test('Player can be kicked by player UID', async () => {
         const endMock = jest.fn();
+
         await sessions.initConnection(token, <unknown>{ end: endMock } as PlayerConnection);
         sessions.kickSessionByPlayer({ uid: '0' });
+
         expect(Object.keys(sessions.playerToToken).length).toBe(0);
         expect(Object.keys(sessions.sessions).length).toBe(0);
         expect(endMock).toBeCalled();

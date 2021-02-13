a214a9b73bbae00684466fa6964c955062ca769a
Meachamp <34523297+Meachamp@users.noreply.github.com>
2021-02-13 06:19:53 -0500

    Implement sessions tracking

--- src/mmsession.ts
+++ src/mmsession.ts
@@ -1,17 +1,83 @@
-import { Player } from './mmplayer';
+import { Player, PlayerUID } from './mmplayer';
+import {MMQClientUpdate, MMQServerUpdate} from './proto/matchmaking_pb';
+import {ServerDuplexStream} from '@grpc/grpc-js';
+import { getPlayerInfo } from './mmapi';
+
+type PlayerConnection = ServerDuplexStream<MMQClientUpdate, MMQServerUpdate>;
+
+interface Session {
+    player: Player;
+    connection: PlayerConnection;
+}
 
 export class MatchMakingSessions {
-    sessions: Record<string, Player>;
+    sessions: Record<string, Session>;
+    playerToToken: Record<PlayerUID, string>;
 
     constructor() {
         this.sessions = {};
+        this.playerToToken = {};
     }
 
-    createSession(token: string, ply: Player) : void {
-        this.sessions[token] = ply;
+    createSession(token: string, ply: Player, conn: PlayerConnection) : void {
+        this.sessions[token] = {
+            player: ply,
+            connection: conn
+        };
+
+        this.playerToToken[ply.uid] = token;
     }
 
     hasSession(token: string) : boolean {
         return this.sessions[token] !== undefined;
+    }
+
+    getSession(token: string) : Session {
+        return this.sessions[token];
+    }
+
+    async initConnection(token: string, call: PlayerConnection) : Promise<boolean> {
+        if(this.hasSession(token)) {
+            this.kickSessionByToken(token);
+        }
+
+        //Retreive player info from the accounts service, or null if the token is invalid.
+        let ply: Player|null = await getPlayerInfo(token);
+        if(ply === null) {
+            call.end();
+            return false;
+        }
+
+        this.createSession(token, ply, call);
+        return true;
+    }
+
+    validateSession(token: string) : Player|null {
+        if(this.hasSession(token)) {
+            return this.getSession(token).player;
+        }
+
+        return null;
+    }
+
+    clearSession(token: string) : void {
+        if (this.hasSession(token)) {
+            const ply: Player = this.sessions[token].player;
+            delete this.playerToToken[ply.uid];
+            delete this.sessions[token];
+        }
+    }
+
+    kickSessionByToken(token: string) : void {
+        const session = this.getSession(token);
+        session.connection.end();
+        this.clearSession(token);
+    }
+
+    kickSessionByPlayer(ply: Player) : void {
+        const token = this.playerToToken[ply.uid];
+        if(token) {
+            this.kickSessionByToken(token);
+        }
     }
 }

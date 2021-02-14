d372d0165a3489f1c4be8d3d915c79680e143ff5
Meachamp <34523297+Meachamp@users.noreply.github.com>
2021-02-14 00:43:44 -0500

    Add inital client test code

---
+++ src/tools/rpc_client.ts
@@ -0,0 +1,20 @@
+import * as grpc from '@grpc/grpc-js';
+import { MatchMakingClient } from '../proto/matchmaking_grpc_pb';
+import { MMQServerUpdate } from '../proto/matchmaking_pb';
+
+const client = new MatchMakingClient("localhost:4000", grpc.credentials.createInsecure());
+
+const md = new grpc.Metadata();
+md.set("token", "test");
+
+const stream = client.queue(md);
+stream.on('data', (data: MMQServerUpdate) => {
+    console.log(data)
+})
+
+stream.on('end', () => {
+    console.log('Stream ended.')
+})
+
+console.log('MM RPC Client')
+

b65c406b238571d16e0a40e5901a03cf1c21f9aa
Meachamp <34523297+Meachamp@users.noreply.github.com>
2021-03-13 02:35:32 -0500

    RPC Client testing

--- src/tools/rpc_client.ts
+++ src/tools/rpc_client.ts
@@ -1,19 +1,27 @@
 import * as grpc from '@grpc/grpc-js';
 import { MatchMakingClient } from '../proto/matchmaking_grpc_pb';
-import { MMQServerUpdate } from '../proto/matchmaking_pb';
+import { MMQClientUpdate, MMQServerUpdate } from '../proto/matchmaking_pb';
+
+console.log('MM RPC Client');
 
 const client = new MatchMakingClient('localhost:4000', grpc.credentials.createInsecure());
 
 const md = new grpc.Metadata();
-md.set('token', 'test');
 
+md.set('token', process.argv[2] || 'test');
+console.log("Using token", md.get('token').toString())
 const stream = client.queue(md);
 stream.on('data', (data: MMQServerUpdate) => {
-    console.log(data);
+    console.log("Matchmaking update")
+    console.log("Status", data.getStatus());
+    console.log("Queue state", data.getQueueState());
+
+    let upd : MMQClientUpdate = new MMQClientUpdate();
+    upd.setRequestedoperation(MMQClientUpdate.QueueOperation.OP_JOIN);
+    stream.write(upd);
 });
 
 stream.on('end', () => {
     console.log('Stream ended.');
 });
 
-console.log('MM RPC Client');
